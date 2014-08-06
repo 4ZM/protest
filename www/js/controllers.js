6776736eacf0723e0c1fb714db73e24c6117b2b2
Anders Sundman <anders@4zm.org>
2014-08-06 13:32:56 +0200

    Uptates to the protest model

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,56 @@
 ﻿angular.module('protest.controllers', [])
 
 .controller('ProtestsCtrl', function($scope, Protests) {
+  Protests.load();
   $scope.protests = Protests.all();
   $scope.refreshing = false;
+
   $scope.refresh = function()
   {
     // Placeholder function - should initiate refresh
     $scope.refreshing = ($scope.refreshing) ? false : true;
+
+    Protests.addProtest(
+      { 
+		id: 0, 
+      	foe: 'Sverigedemokraterna', 
+      	location: 'Kungsörs torg', 
+      	time: '2014-07-29 16:00', 
+      	icon: "img/sd.png", 
+      	url: 'https://www.facebook.com/events/252610284935755/'
+      });
+    Protests.addProtest(
+      { 
+		id: 1, 
+      	foe: 'Svenskarnas Parti', 
+      	location: 'Mynttorget', 
+      	time: '2014-07-19 14:00', 
+      	icon: "img/svp.png"
+      });
+    Protests.addProtest(
+      { 
+      	id: 2, 
+      	foe: 'Israels ockupation', 
+      	location: 'Slottet', 
+      	time: '2014-09-1 14:00'
+      });
+    Protests.addProtest(
+      { 
+      	id: 3, 
+      	foe: 'Sverigedemokraterna', 
+      	location: 'Götaplatsen', 
+      	time: '2014-07-29 16:00', 
+      	icon: "img/sd.png"
+      });
+
+  	$scope.protests = Protests.all();
+	Protests.save($scope.protests);
   }
 })
 
 .controller('ProtestDetailCtrl', function($scope, $stateParams, Protests) {
   $scope.protest = Protests.get($stateParams.protestId);
+  console.log($scope.protest);
 })
 
 .controller('AboutCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,21 +1,38 @@
 ﻿angular.module('protest.services', [])
 
 .factory('Protests', function() {
-  // Some fake testing data
-  var protests = [
-    { id: 0, foe: 'Sverigedemokraterna', location: 'Kungsörs torg', time: '2014-07-29 14:00', icon: "img/sd.png", url: 'https://www.facebook.com/events/252610284935755/'},
-    { id: 1, foe: 'Svenskarnas Parti', location: 'Mynttorget', time: '2014-07-19 14:00', icon: "img/svp.png"  },
-    { id: 2, foe: 'Israels ockupation', location: 'Slottet', time: '2014-09-1 14:00'  },
-    { id: 3, foe: 'Sverigedemokraterna', location: 'Götaplatsen', time: '2014-07-29 16:00', icon: "img/sd.png"  }
-  ];
+  var protests = [];
 
   return {
     all: function() {
       return protests;
     },
-    get: function(protestId) {
-      // Simple index lookup
-      return protests[protestId];
+    get: function(strId) {
+      intId = parseInt(strId)
+      for (i = 0; i < protests.length; ++i) {
+        if (protests[i].id === intId)
+          return protests[i];
+      }
+    },
+    load: function() {
+      var protestsString = window.localStorage['protests'];
+      protests = protestsString ? angular.fromJson(protestsString) : [];
+    },
+    save: function(protests) {
+      window.localStorage['protests'] = angular.toJson(protests);
+    },
+    addProtest: function(newProtest) {
+
+      // Update it if allready exists
+      for (i = 0; i < protests.length; ++i) {
+        if (protests[i].id === newProtest.id) { 
+          protests[i] = newProtest;
+          return;
+        }
+      }
+
+      // Not found, add it to the end
+      protests.push(newProtest);
     }
   }
 });
